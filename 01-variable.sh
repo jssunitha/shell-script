33cdcde3584acebf9d58ad87e8328b3146d92b3e
jssunitha <sunitha130818@gmail.com>
2024-07-19 17:19:49 +0530

    pra

--- 01-variable.sh
+++ 01-variable.sh
@@ -1,5 +1,8 @@
-PERSON1=$1
-PERSON2=$2
 
-echo "$PERSION1 Hello $2 "
-echo "$PERSION2 hello how are you $1"
+
+echo "please enter username:: "
+read USERNAME
+echo "please enter password:"
+read PASSWORD
+
+echo "username is: $USERNAME password is $PASSWORD"
