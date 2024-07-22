73ad8067de085110e4c9bcfad8b209fb426df0df
jssunitha <sunitha130818@gmail.com>
2024-07-22 16:40:07 +0530

    insta

--- installation.sh
+++ installation.sh
@@ -1,18 +1,18 @@
-
+#/bin/bash 
 USERID=$(id -u)
 if [$USERID -ne 0]
 then 
-  echo "please run this script witj root access"
-  exit 1
+    echo "please run this script witj root access"
+    exit 1
 else
-  echo "u are  super user"
+    echo "u are  super user"
  fi
 
- dnf install mysql -y 
+ dnf install mysqll -y 
 
  if [$? -ne 0]
  then 
-   echo "installation of mysql is failure "  
-   exit 1
+     echo "installation of mysql is failure "  
+    exit 1
  else
-    echo "installation is SUCCESS"
+     echo "installation is SUCCESS"
