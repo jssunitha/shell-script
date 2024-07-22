9ce09af4a4d40f3ba526482ba0f25b8dda3e14b5
jssunitha <sunitha130818@gmail.com>
2024-07-22 16:00:27 +0530

    insta

---
+++ installation.sh
@@ -0,0 +1,19 @@
+
+USERID=$(id -u)
+
+if[$USERID -ne 0]
+then 
+  echo "please run this script witj root access"
+  exit 1
+else
+  echo "u are  super user"
+ fi
+
+ dnf install mysql -y 
+
+ if [$? -ne 0]
+ then 
+   echo "installation of mysql is failure "  
+   exit 1
+ else
+    echo "installation FAILURE"
